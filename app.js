cd168c467b667f11231324b88a62986f2508ba9a
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-25 10:31:26 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,83 @@
+// Steps
+// 1. Initialize the app with the express generator
+//   express electric_cars
+//     (creates entire folder structure for using express)
+// 2. Add .gitingore file that will ignore everything inside of node_modules
+//     files wont get added to the git repository
+// 3. Copy the compass boilerplate into the project.
+//     this will give access to compass. We need to change the ruby config file to write to the cerrect location
+//     ../public/stylesheets
+// 4. Init git the repository
+//     git init
+//     git add *
+//     git commit -morgangit push/add origin/open up github desktop
+// 5. npm install ejs --save
+// 6. npm install mongodb --save
+// 7. npm install
+//   this will install express, all it's dependicies, whatever is inside package.jscon
+// 8. run nodemon
+
+
+
+
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
